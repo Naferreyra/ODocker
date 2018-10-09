4d0b9e237515709e483127f442b49bca0906db79
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-10-09 15:58:05 +0200

    [UPD] Update de los archivos para la 11

--- Ubuntu/Buildout/buildout.sh
+++ Ubuntu/Buildout/buildout.sh
@@ -3,5 +3,6 @@
 git config --global user.email "rcerrillo@visiotechsecurity.com"
 git config --global user.name "rcerrillo2"
 virtualenv sandbox --no-setuptools
-sandbox/bin/python bootstrap.py -c devel_buildout.cfg
+sandbox/bin/python3 --version
+sandbox/bin/python3 bootstrap.py -c devel_buildout.cfg
 bin/buildout -c devel_buildout.cfg
